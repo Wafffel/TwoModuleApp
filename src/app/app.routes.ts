b151cf439367c487781ab8d7aac9eb4ab049907b
krzysiek <kristoferski.kw@gmail.com>
2024-04-17 11:04:10 +0200

    naprawiony błąd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Component1Component } from './component1/component1.component';
 import { Module1Module } from './module1/module1.module';
+import { Module1Component } from './module1/module1.component';
 
 export const routes: Routes = [
     {
@@ -14,7 +15,7 @@ export const routes: Routes = [
     },
     {
         path: 'module1',
-        component: Module1Module,
+        component: Module1Component,
         loadChildren: () => import('./module1/module1.module').then(m => m.Module1Module)
     }
 ];
